f3c0b75bb688be728bf6b9b06a26ae695f8024f1
tianyili <tianyityl@gmail.com>
2021-02-24 23:40:15 -0500

    refactoring to multimodule maven project. closes #1

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private guru.springframework.sfgpetclinic.model.PetType petType;
+    private guru.springframework.sfgpetclinic.model.Owner owner;
+    private LocalDate birthDate;
+
+    public guru.springframework.sfgpetclinic.model.PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(guru.springframework.sfgpetclinic.model.PetType petType) {
+        this.petType = petType;
+    }
+
+    public guru.springframework.sfgpetclinic.model.Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(guru.springframework.sfgpetclinic.model.Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
