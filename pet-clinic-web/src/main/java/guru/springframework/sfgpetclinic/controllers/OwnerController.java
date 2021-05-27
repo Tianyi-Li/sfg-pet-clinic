f1c40598867b9b64f5e3cc8bf3a89a0505ff0bdd
tianyili <tianyityl@gmail.com>
2021-05-26 23:10:44 -0400

    Fixed find by like. closes #67

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -47,26 +47,30 @@ public class OwnerController {
     }
 
     @GetMapping
-    public String processFindForm(Owner owner, BindingResult result, Model model) {
-        // allow parameteries GET request for /owners to return all records
+    public String processFindForm(Owner owner, BindingResult result, Model model){
+        // allow parameterless GET request for /owners to return all records
         if (owner.getLastName() == null) {
-            owner.setLastName(""); //empty string signifies broadest possible search
+            owner.setLastName(""); // empty string signifies broadest possible search
         }
 
         // find owners by last name
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
 
         if (results.isEmpty()) {
+            // no owners found
             result.rejectValue("lastName", "notFound", "not found");
             return "owners/findOwners";
         } else if (results.size() == 1) {
+            // 1 owner found
             owner = results.get(0);
             return "redirect:/owners/" + owner.getId();
         } else {
+            // multiple owners found
             model.addAttribute("selections", results);
             return "owners/ownersList";
         }
     }
+
 
 
     @GetMapping("/{ownerId}")
